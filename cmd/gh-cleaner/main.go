2a628902f2d1a5c41e6c259064f24f707cd9d117
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-07-24 23:25:53 -0300

    impl: adding a dry_run mode

--- cmd/gh-cleaner/main.go
+++ cmd/gh-cleaner/main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	bayestheorem "gh-cleaner/internal/bayes_theorem"
 	"gh-cleaner/internal/files"
 	"gh-cleaner/internal/github"
 	"gh-cleaner/internal/prompt"
 	"gh-cleaner/internal/structures"
+	"log"
 )
 
 var (
 	netrcPath string = ""
 	forks     bool   = true
+	dry_run   bool   = false
 	login     structures.Login
 )
 
 func main() {
+	dry_run := flag.Bool("dry-run", false, "Enable dry run mode for training the bayes theorem")
+	flag.Parse()
+
 	netrcPath = files.GetNetrc()
 
 	if netrcPath == "" {
+		log.Println(".netrc not founded")
 		return
 	}
 
 	login = files.MountLogin(netrcPath)
-	prompt.SelectRepo(login, github.GetRepositoriesByToken(login), forks)
+	savedRepos, deletedRepos := files.GetInfoAboutRepo()
+
+	if (savedRepos == nil) && (deletedRepos == nil) {
+		fmt.Println("Run the code with flag --dry-run to create a mock files from this project")
+		return
+	}
+
+	classifier := bayestheorem.GenerateClassifier(deletedRepos, savedRepos)
+
+	prompt.SelectRepo(login, *dry_run, github.GetRepositoriesByToken(login), classifier, forks)
 }
